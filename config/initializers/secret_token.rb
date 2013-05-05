a8363cf5420364da5bfae44e2835adb8972e152d
Leonardo Cardoso <ltc270486@hotmail.com>
2013-05-05 20:52:30 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LES::Application.config.secret_token = '337fc122fbadcdc2525639bdece324af4a70b752c35dd0c17f6e597e9182169c449debbed96126a4b7ab79ec31c45d12e01ab9cfe92359001d13636c04dbb63c'
